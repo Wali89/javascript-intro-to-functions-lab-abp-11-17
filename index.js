94ffdd509da6f2153d49a9f3f54c1943e16a02d3
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-11 14:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function shout (string) {
   console.log (string.toUpperCase);
   return string.toUpperCase ()
 
-};
+}
 
 function whisper (string) {
-  console.log string.toLowerCase ()
+  console.log (string.toLowerCase)
 
-};
+}
 
 function logShout (string) {
   console.log (string.toUpperCase)
-};
+}
